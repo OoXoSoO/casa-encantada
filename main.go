6410f4f6c5c526dab86fcfcb65b0fd35d01bf282
jose.diaz <jose.diaz@xmltravelgate.com>
2023-10-24 08:49:02 +0200

    refactor

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import "fmt"
 
 func main() {
-
 	sc := NewScreen()
 	sc.Print()
 	for !sc.Game() {
@@ -11,5 +10,4 @@ func main() {
 		sc.Print()
 	}
 	fmt.Println("Congratulaciones")
-
 }
